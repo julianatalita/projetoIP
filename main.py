6852ed9a296a768827e2550862c5cb26b70a9c86
CatPlays Games <wsn@cin.ufpe.br>
2023-09-12 20:11:07 -0300

    funcoes basicas pg

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import pygame as pg
-
+from functions import update_screen
 
 screen = pg.display.set_mode((1600,800))
 
+frame_count = 0
+
 while True:
+
+    # Obter as teclas pressionadas
     keys = pg.key.get_pressed()
+
+    # Sair do jogo ao fechar a janela
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+    
+    # Update na tela (duh)
+    update_screen()
+
+    # Contar os frames e rodar a 60FPS
+    frame_count += 1
+    pg.time.Clock.tick(60)
