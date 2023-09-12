6999518c8b564ae8425efb3248688dd64daa71f3
CatPlays Games <wsn@cin.ufpe.br>
2023-09-12 20:02:05 -0300

    tela preta

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("oi")
+import pygame as pg
+
+
+screen = pg.display.set_mode((1600,800))
+
+while True:
+    keys = pg.key.get_pressed()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
